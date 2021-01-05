01e87368ef8763b6cc3be5725c5325f1d6c46f49
Jun Kimura <junkxdev@gmail.com>
2021-01-06 00:54:09 +0900

    add ChainConfigI interface
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- Makefile
+++ Makefile
@@ -5,3 +5,8 @@ build:
 .PHONY: test
 test:
 	go test -v ./...
+
+.PHONY: proto-gen
+proto-gen:
+	@echo "Generating Protobuf files"
+	docker run -v $(CURDIR):/workspace --workdir /workspace tendermintdev/sdk-proto-gen sh ./scripts/protocgen.sh
--- config/marshaler.go
+++ config/marshaler.go
@@ -1,11 +1,58 @@
 package config
 
-import "encoding/json"
+import (
+	"encoding/json"
+
+	"github.com/gogo/protobuf/proto"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+)
 
 func MarshalJSON(config Config) ([]byte, error) {
 	return json.Marshal(config)
 }
 
-func UnmarshalJSON(bz []byte, config *Config) error {
-	return json.Unmarshal(bz, config)
+func UnmarshalJSON(m codec.Marshaler, bz []byte, config *Config) error {
+	if err := json.Unmarshal(bz, config); err != nil {
+		return err
+	}
+	for _, c := range config.Chains {
+		var cfg ChainConfigI
+		if err := UnmarshalJSONAny(m, &cfg, c); err != nil {
+			return err
+		}
+		config.chains = append(config.chains, cfg.GetChain())
+	}
+	return nil
+}
+
+// MarshalJSONAny is a convenience function for packing the provided value in an
+// Any and then proto marshaling it to bytes
+func MarshalJSONAny(m codec.JSONMarshaler, msg proto.Message) ([]byte, error) {
+	any := &types.Any{}
+	err := any.Pack(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	return m.MarshalJSON(any)
+}
+
+// UnmarshalJSONAny is a convenience function for proto unmarshaling an Any from
+// bz and then unpacking it to the interface pointer passed in as iface using
+// the provided AnyUnpacker or returning an error
+//
+// Ex:
+//		var x MyInterface
+//		err := UnmarshalJSONAny(unpacker, &x, bz)
+func UnmarshalJSONAny(m codec.Marshaler, iface interface{}, bz []byte) error {
+	any := &types.Any{}
+
+	err := m.UnmarshalJSON(bz, any)
+	if err != nil {
+		return err
+	}
+
+	return m.UnpackAny(any, iface)
 }
