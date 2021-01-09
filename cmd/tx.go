52b65a9f81285d1e6298f5fa13e0a738a3f9c89b
Jun Kimura <junkxdev@gmail.com>
2021-01-09 15:20:47 +0900

    add connection command skeleton
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- cmd/tx.go
+++ cmd/tx.go
@@ -11,9 +11,8 @@ import (
 // transactionCmd represents the tx command
 func transactionCmd(ctx *config.Context) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "transact",
-		Aliases: []string{"tx"},
-		Short:   "IBC Transaction Commands",
+		Use:   "tx",
+		Short: "IBC Transaction Commands",
 		Long: strings.TrimSpace(`Commands to create IBC transactions on configured chains. 
 		Most of these commands take a '[path]' argument. Make sure:
 	1. Chains are properly configured to relay over by using the 'rly chains list' command
@@ -22,6 +21,7 @@ func transactionCmd(ctx *config.Context) *cobra.Command {
 
 	cmd.AddCommand(
 		createClientsCmd(ctx),
+		createConnectionCmd(ctx),
 	)
 
 	return cmd
@@ -29,9 +29,8 @@ func transactionCmd(ctx *config.Context) *cobra.Command {
 
 func createClientsCmd(ctx *config.Context) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "clients [path-name]",
-		Aliases: []string{"clnts"},
-		Short:   "create a clients between two configured chains with a configured path",
+		Use:   "clients [path-name]",
+		Short: "create a clients between two configured chains with a configured path",
 		Long: "Creates a working ibc client for chain configured on each end of the" +
 			" path by querying headers from each chain and then sending the corresponding create-client messages",
 		Args: cobra.ExactArgs(1),
@@ -53,4 +52,37 @@ func createClientsCmd(ctx *config.Context) *cobra.Command {
 		},
 	}
 	return cmd
+}
+
+func createConnectionCmd(ctx *config.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "connection [path-name]",
+		Short: "create a connection between two configured chains with a configured path",
+		Long: strings.TrimSpace(`This command is meant to be used to repair or create 
+		a connection between two chains with a configured path in the config file`),
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			c, src, dst, err := ctx.Config.ChainsFromPath(args[0])
+			if err != nil {
+				return err
+			}
+
+			to, err := getTimeout(cmd)
+			if err != nil {
+				return err
+			}
+
+			// ensure that keys exist
+			if _, err = c[src].GetAddress(); err != nil {
+				return err
+			}
+			if _, err = c[dst].GetAddress(); err != nil {
+				return err
+			}
+
+			return core.CreateConnections(c[src], c[dst], to)
+		},
+	}
+
+	return timeoutFlag(cmd)
 }
---
+++ core/connection.go
@@ -0,0 +1,9 @@
+package core
+
+import (
+	"time"
+)
+
+func CreateConnections(src, dst ChainI, to time.Duration) error {
+	return nil
+}
