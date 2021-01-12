9435859a46c95d7ed13695f81e6e842680f7f4ca
Jun Kimura <junkxdev@gmail.com>
2021-01-12 12:10:25 +0900

    add connection querier
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- cmd/query.go
+++ cmd/query.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/x/ibc/core/exported"
 	"github.com/datachainlab/relayer/config"
 	"github.com/spf13/cobra"
 )
@@ -18,6 +19,7 @@ func queryCmd(ctx *config.Context) *cobra.Command {
 
 	cmd.AddCommand(
 		queryClientCmd(ctx),
+		queryConnection(ctx),
 	)
 
 	return cmd
@@ -50,10 +52,43 @@ func queryClientCmd(ctx *config.Context) *cobra.Command {
 			if err != nil {
 				return err
 			}
-			fmt.Println(string(res.ClientState.Value))
+			var cs exported.ClientState
+			if err := c.Marshaler().UnpackAny(res.ClientState, &cs); err != nil {
+				return err
+			}
+			fmt.Println(cs)
 			return nil
 		},
 	}
 
 	return heightFlag(cmd)
+}
+
+func queryConnection(ctx *config.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "connection [path-name] [chain-id]",
+		Short: "Query the connection state for the given connection id",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			chains, _, _, err := ctx.Config.ChainsFromPath(args[0])
+			if err != nil {
+				return err
+			}
+			c := chains[args[1]]
+
+			height, err := c.QueryLatestHeight()
+			if err != nil {
+				return err
+			}
+
+			res, err := c.QueryConnection(height, false)
+			if err != nil {
+				return err
+			}
+			fmt.Println(res.Connection.String())
+			return nil
+		},
+	}
+
+	return cmd
 }
