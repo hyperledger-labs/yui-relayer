967f9f421338686df309b9f3a1d96998a104b9b3
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2021-01-26 23:06:53 +0900

    corda: implement most of query/tx functions for interacting with Corda-IBC

--- chains/corda/tx.go
+++ chains/corda/tx.go
@@ -1,13 +1,152 @@
 package corda
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	transfertypes "github.com/cosmos/cosmos-sdk/x/ibc/applications/transfer/types"
+	clienttypes "github.com/cosmos/cosmos-sdk/x/ibc/core/02-client/types"
+	conntypes "github.com/cosmos/cosmos-sdk/x/ibc/core/03-connection/types"
+	chantypes "github.com/cosmos/cosmos-sdk/x/ibc/core/04-channel/types"
+)
 
 func (c *Chain) SendMsgs(msgs []sdk.Msg) ([]byte, error) {
-	panic("not implemented error")
+	for _, msg := range msgs {
+		var err error
+		switch msg := msg.(type) {
+		case *clienttypes.MsgCreateClient:
+			err = c.client.createClient(msg)
+		case *conntypes.MsgConnectionOpenInit:
+			err = c.client.connOpenInit(msg)
+		case *conntypes.MsgConnectionOpenTry:
+			err = c.client.connOpenTry(msg)
+		case *conntypes.MsgConnectionOpenAck:
+			err = c.client.connOpenAck(msg)
+		case *conntypes.MsgConnectionOpenConfirm:
+			err = c.client.connOpenConfirm(msg)
+		case *chantypes.MsgChannelOpenInit:
+			err = c.client.chanOpenInit(msg)
+		case *chantypes.MsgChannelOpenTry:
+			err = c.client.chanOpenTry(msg)
+		case *chantypes.MsgChannelOpenAck:
+			err = c.client.chanOpenAck(msg)
+		case *chantypes.MsgChannelOpenConfirm:
+			err = c.client.chanOpenConfirm(msg)
+		case *chantypes.MsgRecvPacket:
+			err = c.client.recvPacket(msg)
+		case *chantypes.MsgAcknowledgement:
+			err = c.client.acknowledgement(msg)
+		case *transfertypes.MsgTransfer:
+			err = c.client.transfer(msg)
+		default:
+			panic("illegal msg type")
+		}
+		if err != nil {
+			return nil, err
+		}
+	}
+	return nil, nil
 }
 
 // Send sends msgs to the chain and logging a result of it
 // It returns a boolean value whether the result is success
 func (c *Chain) Send(msgs []sdk.Msg) bool {
-	panic("not implemented error")
+	_, err := c.SendMsgs(msgs)
+	return err == nil
+}
+
+func (cic *cordaIbcClient) createClient(msg *clienttypes.MsgCreateClient) error {
+	_, err := cic.clientTx.CreateClient(
+		context.TODO(),
+		msg,
+	)
+	return err
+}
+
+func (cic *cordaIbcClient) connOpenInit(msg *conntypes.MsgConnectionOpenInit) error {
+	_, err := cic.connTx.ConnectionOpenInit(
+		context.TODO(),
+		msg,
+	)
+	return err
+}
+
+func (cic *cordaIbcClient) connOpenTry(msg *conntypes.MsgConnectionOpenTry) error {
+	_, err := cic.connTx.ConnectionOpenTry(
+		context.TODO(),
+		msg,
+	)
+	return err
+}
+
+func (cic *cordaIbcClient) connOpenAck(msg *conntypes.MsgConnectionOpenAck) error {
+	_, err := cic.connTx.ConnectionOpenAck(
+		context.TODO(),
+		msg,
+	)
+	return err
+}
+
+func (cic *cordaIbcClient) connOpenConfirm(msg *conntypes.MsgConnectionOpenConfirm) error {
+	_, err := cic.connTx.ConnectionOpenConfirm(
+		context.TODO(),
+		msg,
+	)
+	return err
+}
+
+func (cic *cordaIbcClient) chanOpenInit(msg *chantypes.MsgChannelOpenInit) error {
+	_, err := cic.chanTx.ChannelOpenInit(
+		context.TODO(),
+		msg,
+	)
+	return err
+}
+
+func (cic *cordaIbcClient) chanOpenTry(msg *chantypes.MsgChannelOpenTry) error {
+	_, err := cic.chanTx.ChannelOpenTry(
+		context.TODO(),
+		msg,
+	)
+	return err
+}
+
+func (cic *cordaIbcClient) chanOpenAck(msg *chantypes.MsgChannelOpenAck) error {
+	_, err := cic.chanTx.ChannelOpenAck(
+		context.TODO(),
+		msg,
+	)
+	return err
+}
+
+func (cic *cordaIbcClient) chanOpenConfirm(msg *chantypes.MsgChannelOpenConfirm) error {
+	_, err := cic.chanTx.ChannelOpenConfirm(
+		context.TODO(),
+		msg,
+	)
+	return err
+}
+
+func (cic *cordaIbcClient) recvPacket(msg *chantypes.MsgRecvPacket) error {
+	_, err := cic.chanTx.RecvPacket(
+		context.TODO(),
+		msg,
+	)
+	return err
+}
+
+func (cic *cordaIbcClient) acknowledgement(msg *chantypes.MsgAcknowledgement) error {
+	_, err := cic.chanTx.Acknowledgement(
+		context.TODO(),
+		msg,
+	)
+	return err
+}
+
+func (cic *cordaIbcClient) transfer(msg *transfertypes.MsgTransfer) error {
+	_, err := cic.transferTx.Transfer(
+		context.TODO(),
+		msg,
+	)
+	return err
 }
