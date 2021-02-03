9b03019276771b240781a96c23706b94f120fa91
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2021-02-03 20:43:22 +0900

    corda: use proto files from datachaianlab/corda-ibc

--- chains/corda/create-client.go
+++ chains/corda/create-client.go
@@ -7,6 +7,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	clienttypes "github.com/cosmos/cosmos-sdk/x/ibc/core/02-client/types"
 	"github.com/datachainlab/relayer/core"
+	"github.com/datachainlab/relayer/jp_datachain_corda_ibc_grpc"
 )
 
 // MakeMsgCreateClient creates a CreateClientMsg to this chain
@@ -14,17 +15,17 @@ func (c *Chain) MakeMsgCreateClient(clientID string, dstHeader core.HeaderI, sig
 	// information for building consensus state can be obtained from host state
 	host, err := c.client.hostAndBankQuery.QueryHost(
 		context.TODO(),
-		&QueryHostRequest{},
+		&jp_datachain_corda_ibc_grpc.QueryHostRequest{},
 	)
 	if err != nil {
 		return nil, err
 	}
-	consensusState := ConsensusState{
+	consensusState := jp_datachain_corda_ibc_grpc.ConsensusState{
 		BaseId:    host.BaseId,
 		NotaryKey: host.Notary.OwningKey,
 	}
 
-	if anyClientState, err := types.NewAnyWithValue(&ClientState{}); err != nil {
+	if anyClientState, err := types.NewAnyWithValue(&jp_datachain_corda_ibc_grpc.ClientState{}); err != nil {
 		return nil, err
 	} else if anyConsensusState, err := types.NewAnyWithValue(&consensusState); err != nil {
 		return nil, err
