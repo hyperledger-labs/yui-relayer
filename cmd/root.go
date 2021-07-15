5cf7431b15a91840c8cef668f02ce6b16fc86d90
Jun Kimura <jun.kimura@datachain.jp>
2021-07-15 16:47:03 +0900

    fix to Execute() returns an error
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- cmd/root.go
+++ cmd/root.go
@@ -14,21 +14,20 @@ import (
 )
 
 var (
-	homePath string
-	debug    bool
-	// configInstance *config.Config
+	homePath    string
+	debug       bool
 	defaultHome = os.ExpandEnv("$HOME/.urelayer")
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "uly",
-	Short: "This application relays data between configured IBC enabled chains",
-}
-
 // Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute(modules ...config.ModuleI) {
+// It can support any chain by giving modules.
+func Execute(modules ...config.ModuleI) error {
+	// rootCmd represents the base command when called without any subcommands
+	var rootCmd = &cobra.Command{
+		Use:   "uly",
+		Short: "This application relays data between configured IBC enabled chains",
+	}
+
 	cobra.EnableCommandSorting = false
 	rootCmd.SilenceUsage = true
 
@@ -36,18 +35,22 @@ func Execute(modules ...config.ModuleI) {
 	rootCmd.PersistentFlags().StringVar(&homePath, flags.FlagHome, defaultHome, "set home directory")
 	rootCmd.PersistentFlags().BoolVarP(&debug, "debug", "d", false, "debug output")
 	if err := viper.BindPFlag(flags.FlagHome, rootCmd.Flags().Lookup(flags.FlagHome)); err != nil {
-		panic(err)
+		return err
 	}
 	if err := viper.BindPFlag("debug", rootCmd.Flags().Lookup("debug")); err != nil {
-		panic(err)
+		return err
 	}
+
+	// Register interfaces
 
 	ec := core.MakeEncodingConfig()
 	for _, module := range modules {
 		module.RegisterInterfaces(ec.InterfaceRegistry)
 	}
 	ctx := &config.Context{Config: &config.Config{}, Marshaler: ec.Marshaler}
+
 	// Register subcommands
+
 	rootCmd.AddCommand(
 		configCmd(ctx),
 		chainsCmd(ctx),
@@ -62,6 +65,7 @@ func Execute(modules ...config.ModuleI) {
 			rootCmd.AddCommand(cmd)
 		}
 	}
+
 	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, _ []string) error {
 		// reads `homeDir/config/config.yaml` into `var config *Config` before each command
 		if err := viper.BindPFlags(cmd.Flags()); err != nil {
@@ -70,9 +74,7 @@ func Execute(modules ...config.ModuleI) {
 		return initConfig(ctx, rootCmd)
 	}
 
-	if err := rootCmd.Execute(); err != nil {
-		os.Exit(1)
-	}
+	return rootCmd.Execute()
 }
 
 // readLineFromBuf reads one line from stdin.
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	corda "github.com/hyperledger-labs/yui-relayer/chains/corda/module"
 	fabric "github.com/hyperledger-labs/yui-relayer/chains/fabric/module"
 	tendermint "github.com/hyperledger-labs/yui-relayer/chains/tendermint/module"
@@ -8,9 +10,11 @@ import (
 )
 
 func main() {
-	cmd.Execute(
+	if err := cmd.Execute(
 		tendermint.Module{},
 		fabric.Module{},
 		corda.Module{},
-	)
+	); err != nil {
+		log.Fatal(err)
+	}
 }
