73fc1be344c727bfa47d77a8f46acb6c136ce309
Jun Kimura <jun.kimura@datachain.jp>
2021-07-21 13:40:35 +0900

    corda,fabric: register the interfaces of the light-client
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- chains/corda/codec.go
+++ chains/corda/codec.go
@@ -2,11 +2,14 @@ package corda
 
 import (
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cordatypes "github.com/hyperledger-labs/yui-corda-ibc/go/x/ibc/light-clients/xx-corda/types"
 	"github.com/hyperledger-labs/yui-relayer/core"
 )
 
 // RegisterInterfaces register the module interfaces to protobuf Any.
 func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
+	cordatypes.RegisterInterfaces(registry)
+
 	registry.RegisterImplementations(
 		(*core.ChainConfigI)(nil),
 		&ChainConfig{},
--- chains/fabric/codec.go
+++ chains/fabric/codec.go
@@ -2,14 +2,14 @@ package fabric
 
 import (
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
-	"github.com/hyperledger-labs/yui-fabric-ibc/x/ibc/light-clients/xx-fabric/types"
+	fabrictypes "github.com/hyperledger-labs/yui-fabric-ibc/x/ibc/light-clients/xx-fabric/types"
 	"github.com/hyperledger-labs/yui-relayer/core"
 )
 
 // RegisterInterfaces register the module interfaces to protobuf
 // Any.
 func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
-	types.RegisterInterfaces(registry)
+	fabrictypes.RegisterInterfaces(registry)
 
 	registry.RegisterImplementations(
 		(*core.ChainConfigI)(nil),
