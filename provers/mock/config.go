9b52d82390fd2c6c5fadd80f94973aa5824aff8f
Jun Kimura <jun.kimura@datachain.jp>
2021-07-30 11:26:37 +0900

    mock: fix to use a static number as sequence
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- provers/mock/config.go
+++ provers/mock/config.go
@@ -1,13 +1,11 @@
 package mock
 
 import (
-	"time"
-
 	"github.com/hyperledger-labs/yui-relayer/core"
 )
 
 var _ core.ProverConfigI = (*ProverConfig)(nil)
 
 func (c *ProverConfig) Build(chain core.ChainI) (core.ProverI, error) {
-	return NewProver(chain, uint64(time.Now().UnixNano())), nil
+	return NewProver(chain, 1), nil
 }
