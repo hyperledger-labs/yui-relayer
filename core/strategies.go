42d109c333b607bc6f19fb99a54349638b22f297
Masanori Yoshida <masanori.yoshida@datachain.jp>
2023-08-04 23:58:49 +0900

    write description comments for StrategyI's functions
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- core/strategies.go
+++ core/strategies.go
@@ -7,11 +7,26 @@ import (
 
 // StrategyI defines
 type StrategyI interface {
+	// GetType returns the type of the strategy
 	GetType() string
+
+	// SetupRelay performs chain-specific setup for `src` and `dst` before starting the relay
 	SetupRelay(ctx context.Context, src, dst *ProvableChain) error
+
+	// UnrelayedPackets returns packets to execute RecvPacket to on `src` and `dst`.
+	// `scanFinalizedEvents` decides whether this function gathers SendPacket events up to the latest FINALIZED height or the latest height.
+	// `scanFinalizedRelays` decides whether this function returns packets that are not received yet at the latest FINALIZED height or the latest height.
 	UnrelayedPackets(src, dst *ProvableChain, sh SyncHeaders, scanFinalizedEvents, scanFinalizedRelays bool) (*RelayPackets, error)
+
+	// RelayPackets executes RecvPacket to the packets contained in `rp` on both chains (`src` and `dst`).
 	RelayPackets(src, dst *ProvableChain, rp *RelayPackets, sh SyncHeaders) error
+
+	// UnrelayedAcknowledgements returns packets to execute AcknowledgePacket to on `src` and `dst`.
+	// `scanFinalizedEvents` decides whether this function gathers RecvPacket/WriteAcknowledgement events up to the latest FINALIZED height or the latest height.
+	// `scanFinalizedRelays` decides whether this function returns packets of which acks are not received yet at the latest FINALIZED height or the latest height.
 	UnrelayedAcknowledgements(src, dst *ProvableChain, sh SyncHeaders, scanFinalizedEvents, scanFinalizedRelays bool) (*RelayPackets, error)
+
+	// RelayAcknowledgements executes AcknowledgePacket to the packets contained in `rp` on both chains (`src` and `dst`).
 	RelayAcknowledgements(src, dst *ProvableChain, ra *RelayPackets, sh SyncHeaders) error
 }
 
