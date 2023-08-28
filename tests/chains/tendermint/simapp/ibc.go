dd8005b881597864ed08cc8f3873e8799251d9f8
Masanori Yoshida <masanori.yoshida@datachain.jp>
2023-08-28 13:20:41 +0900

    override `GetSelfConsensusState` in simd for e2e testing
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- tests/chains/tendermint/simapp/ibc.go
+++ tests/chains/tendermint/simapp/ibc.go
@@ -1,24 +1,24 @@
 package simapp
 
 import (
-	"fmt"
-
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	clientkeeper "github.com/cosmos/ibc-go/v7/modules/core/02-client/keeper"
+	clienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
 	connectionkeeper "github.com/cosmos/ibc-go/v7/modules/core/03-connection/keeper"
 	connectiontypes "github.com/cosmos/ibc-go/v7/modules/core/03-connection/types"
 	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
 	"github.com/cosmos/ibc-go/v7/modules/core/exported"
 	ibckeeper "github.com/cosmos/ibc-go/v7/modules/core/keeper"
-	tenderminttypes "github.com/cosmos/ibc-go/v7/modules/light-clients/07-tendermint"
 	mocktypes "github.com/datachainlab/ibc-mock-client/modules/light-clients/xx-mock/types"
 )
 
-func overrideIBCClientKeeper(k ibckeeper.Keeper, cdc codec.BinaryCodec, key storetypes.StoreKey, paramSpace paramtypes.Subspace) *ibckeeper.Keeper {
-	clientKeeper := NewClientKeeper(k.ClientKeeper)
+func overrideIBCClientKeeper(k ibckeeper.Keeper, cdc codec.BinaryCodec, key storetypes.StoreKey, paramSpace paramtypes.Subspace, stakingKeeper *stakingkeeper.Keeper) *ibckeeper.Keeper {
+	clientKeeper := NewClientKeeper(k.ClientKeeper, stakingKeeper)
 	k.ConnectionKeeper = connectionkeeper.NewKeeper(cdc, key, paramSpace, clientKeeper)
 	return &k
 }
@@ -26,23 +26,37 @@ func overrideIBCClientKeeper(k ibckeeper.Keeper, cdc codec.BinaryCodec, key stor
 var _ connectiontypes.ClientKeeper = (*ClientKeeper)(nil)
 var _ channeltypes.ClientKeeper = (*ClientKeeper)(nil)
 
-// ClientKeeper override `ValidateSelfClient` in the keeper of ibc-client
+// ClientKeeper override `ValidateSelfClient` and `GetSelfConsensusState` in the keeper of ibc-client
 // original method doesn't yet support a consensus state for general client
 type ClientKeeper struct {
 	clientkeeper.Keeper
+	stakingKeeper *stakingkeeper.Keeper
 }
 
-func NewClientKeeper(k clientkeeper.Keeper) ClientKeeper {
-	return ClientKeeper{Keeper: k}
+func NewClientKeeper(k clientkeeper.Keeper, stakingKeeper *stakingkeeper.Keeper) ClientKeeper {
+	return ClientKeeper{Keeper: k, stakingKeeper: stakingKeeper}
 }
 
 func (k ClientKeeper) ValidateSelfClient(ctx sdk.Context, clientState exported.ClientState) error {
-	switch cs := clientState.(type) {
-	case *tenderminttypes.ClientState:
-		return k.Keeper.ValidateSelfClient(ctx, cs)
-	case *mocktypes.ClientState:
-		return nil
-	default:
-		return fmt.Errorf("unexpected client state type: %T", cs)
+	return nil
+}
+
+func (k ClientKeeper) GetSelfConsensusState(ctx sdk.Context, height exported.Height) (exported.ConsensusState, error) {
+	selfHeight, ok := height.(clienttypes.Height)
+	if !ok {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "expected %T, got %T", clienttypes.Height{}, height)
 	}
+	// check that height revision matches chainID revision
+	revision := clienttypes.ParseChainID(ctx.ChainID())
+	if revision != height.GetRevisionNumber() {
+		return nil, sdkerrors.Wrapf(clienttypes.ErrInvalidHeight, "chainID revision number does not match height revision number: expected %d, got %d", revision, height.GetRevisionNumber())
+	}
+	histInfo, found := k.stakingKeeper.GetHistoricalInfo(ctx, int64(selfHeight.RevisionHeight))
+	if !found {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrNotFound, "no historical info found at height %d", selfHeight.RevisionHeight)
+	}
+	consensusState := &mocktypes.ConsensusState{
+		Timestamp: uint64(histInfo.Header.Time.UnixNano()),
+	}
+	return consensusState, nil
 }
