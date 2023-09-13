236f8d37ed099600627a1ae46bedc6e32933d48f
Masanori Yoshida <masanori.yoshida@datachain.jp>
2023-09-13 11:23:35 +0900

    add utility funciton `GetFinalizedMsgResult`
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- core/send.go
+++ core/send.go
@@ -3,9 +3,11 @@ package core
 import (
 	"fmt"
 
+	retry "github.com/avast/retry-go"
 	"github.com/cosmos/cosmos-sdk/types"
 )
 
+// SendMsgsAndCheckResult is an utility function that executes `Chain::SendMsgs` and checks the results of all the messages.
 func SendMsgsAndCheckResult(chain Chain, msgs []types.Msg) error {
 	ids, err := chain.SendMsgs(msgs)
 	if err != nil {
@@ -20,4 +22,35 @@ func SendMsgsAndCheckResult(chain Chain, msgs []types.Msg) error {
 		}
 	}
 	return nil
+}
+
+// GetFinalizedMsgResult is an utility function that waits for the finalization of the message execution and then returns the result.
+func GetFinalizedMsgResult(chain ProvableChain, msgID MsgID) (MsgResult, error) {
+	var msgRes MsgResult
+	if err := retry.Do(func() error {
+		var err error
+
+		// query LFH for each retry because it can proceed.
+		header, err := chain.GetLatestFinalizedHeader()
+		if err != nil {
+			return fmt.Errorf("failed to get latest finalized header: %v", err)
+		}
+
+		// query MsgResult for each retry because it can be included in a different block because of reorg
+		msgRes, err = chain.GetMsgResult(msgID)
+		if err != nil {
+			return fmt.Errorf("failed to get messge result: %v", err)
+		}
+
+		// check whether the block that includes the message has been finalized, or not
+		if msgRes.BlockHeight().GT(header.GetHeight()) {
+			return fmt.Errorf("message_height(%v) > latest_finalized_height(%v)", msgRes.BlockHeight(), header.GetHeight())
+		}
+
+		return nil
+	}, rtyAtt, rtyDel, rtyErr); err != nil {
+		return nil, err
+	} else {
+		return msgRes, nil
+	}
 }
