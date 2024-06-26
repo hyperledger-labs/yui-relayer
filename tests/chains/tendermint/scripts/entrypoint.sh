415236965babeb872314166e79c214f690cc0562
Masanori Yoshida <masanori.yoshida@datachain.jp>
2024-06-26 10:49:58 +0900

    fix simapp to use the relayer account as authority for IBC
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- tests/chains/tendermint/scripts/entrypoint.sh
+++ tests/chains/tendermint/scripts/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+export IBC_AUTHORITY=`jq -r .address /root/${CHAINDIR}/${CHAINID}/key_seed.json`
+
 simd --home /root/${CHAINDIR}/${CHAINID} start --pruning=nothing --grpc.address="0.0.0.0:${GRPCPORT}"
