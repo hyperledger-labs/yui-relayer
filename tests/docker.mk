6d54839fc7c7dbc895bbc1b4704ac3a8f10a9a9e
Daisuke Kanda <dai1975@gmail.com>
2024-08-08 05:26:42 +0000

    use gogoprogo.jsobpb.Marshaler to output object
    Signed-off-by: Daisuke Kanda <dai1975@gmail.com>

--- tests/docker.mk
+++ tests/docker.mk
@@ -1,5 +1,5 @@
 DOCKER         ?= docker
-DOCKER_COMPOSE ?= docker-compose
+DOCKER_COMPOSE ?= docker compose
 DOCKER_REPO    ?= ""
 DOCKER_TAG     ?= latest
 DOCKER_BUILD   ?= $(DOCKER) build --rm --no-cache --pull
