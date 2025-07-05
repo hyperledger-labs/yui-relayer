5bd3a07b8af1ec27ef853e66c9c6a71cb5b838cb
abicky <takeshi.arabiki@gmail.com>
2025-07-05 21:41:32 +0900

    Add version information support
    This commit introduces --version flag to the root command,
    allowing users to check the version of the tool.
    It also enhances the modules show command to include detailed
    information.
    
    Signed-off-by: abicky <takeshi.arabiki@gmail.com>

--- cmd/module.go
+++ cmd/module.go
@@ -2,7 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"reflect"
+	"runtime/debug"
+	"slices"
 	"sort"
+	"strings"
 
 	"github.com/hyperledger-labs/yui-relayer/config"
 	"github.com/spf13/cobra"
@@ -27,16 +31,37 @@ func showModulesCmd(ctx *config.Context) *cobra.Command {
 		Use:   "show",
 		Short: "Shows a list of modules included in the relayer",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			names := make([]string, len(ctx.Modules))
+			modules := make([]string, len(ctx.Modules))
+			bi, _ := debug.ReadBuildInfo()
 			for i, m := range ctx.Modules {
-				names[i] = m.Name()
+				modules[i] = m.Name() + " " + retrieveModuleInfo(bi, m)
 			}
-			sort.Strings(names)
-			for _, name := range names {
-				fmt.Printf("%v\n", name)
+			sort.Strings(modules)
+			for _, module := range modules {
+				fmt.Printf("%v\n", module)
 			}
 			return nil
 		},
 	}
 	return cmd
+}
+
+func retrieveModuleInfo(info *debug.BuildInfo, m config.ModuleI) string {
+	if info == nil {
+		return ""
+	}
+
+	pkgPath := reflect.TypeOf(m).PkgPath()
+	if strings.HasPrefix(pkgPath, info.Main.Path) {
+		return info.Main.Path + " " + info.Main.Version
+	}
+
+	i := slices.IndexFunc(info.Deps, func(dm *debug.Module) bool {
+		return strings.HasPrefix(pkgPath, dm.Path)
+	})
+	if i == -1 {
+		return ""
+	}
+
+	return info.Deps[i].Path + " " + info.Deps[i].Version
 }
