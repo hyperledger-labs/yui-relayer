5326276fdf773bfa38f256ba7a25f42dc3385101
Daisuke Kanda <daisuke.kanda@datachain.jp>
2025-10-08 08:49:57 +0000

    re-run test
    Signed-off-by: Daisuke Kanda <daisuke.kanda@datachain.jp>

--- provers/debug/prover.go
+++ provers/debug/prover.go
@@ -29,7 +29,7 @@ func debugFakeLost(ctx context.Context, chain core.Chain, queryHeight exported.H
 
 		qh := int64(queryHeight.GetRevisionHeight())
 		if qh == 0 {
-			// 0 query height means latest height
+			// 0 means query latest height
 			return nil
 		}
 
